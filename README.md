e4034a5ba122dadf70dc7684d7bf0676a4b7803f
Dan Schwartz <dpschwartz@gmail.com>
2015-11-09 13:02:17 -0800

    Initial commit.

---
+++ README.md
@@ -0,0 +1,5 @@
+Vote Tracker
+
+by Dan Schwartz
+
+It's the fun new game: vote for your favorite US Supreme Court Justices!
---
+++ script.js
@@ -0,0 +1,11 @@
+var allPhotos = [];
+
+function picture (name, title, tenure, path) {
+  this.name = name;
+  this.title = title;
+  this.tenure = tenure;
+  this.path = path;
+  votes???
+}
+
+function tracker ()
